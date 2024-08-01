792cebfd42659f6507a58c742bca66f22b2ca28c
CGM123sherry <g.christinemurithi@gmail.com>
2024-08-01 06:57:21 +0300

    completed the hooks jsx lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { image } from "../data/data";
+
+const imageUrl = "https://i.imgur.com/mV8PQxj.gif";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>This is a brief description about me.</p>
+      <img src={imageUrl} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
